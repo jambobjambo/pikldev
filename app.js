f053e07de66d8ba184f51f547a331e233e0ae766
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-22 07:10:41 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -10,14 +10,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", function(req,res){
-    res.send('Hello World!');
+    res.status(200).send('Hello World!');
 });
 
 app.use("/product", function(req,res){
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ 'product' : 'Mens T shirt' }));
+    res.status(200).send(JSON.stringify({ 'product' : 'Mens T shirt' }));
 });
 
-app.listen(3000,function(){
-    console.log("Live at Port 3000");
+// Start the server
+var server = app.listen(process.env.PORT || '8080', function () {
+    console.log('App listening on port %s', server.address().port);
+    console.log('Press Ctrl+C to quit.');
 });
