b80e03bfcac94922e89cf9902e15355df5ee58c2
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-28 12:19:05 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
 var router = express.Router();
-var PythonShell = require('python-shell');
+//var PythonShell = require('python-shell');
 
 var options = {
     mode: 'text',
@@ -19,14 +19,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req,res){
-    PythonShell.run('../../piklimageprediction/label.py', options, function (err, results) {
+    /*PythonShell.run('../../piklimageprediction/label.py', options, function (err, results) {
         if (err) throw err;
         // results is an array consisting of messages collected during execution
         console.log('results: %j', results);
-    });
-    var image = req.query.image
-    //res.setHeader('Content-Type', 'application/json');
-    //res.send(JSON.stringify({ 'product' : 'Mens T shirt' }));
+    });*/
+    //var image = req.query.image
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 'product' : 'Mens T shirt' }));
 });
 
 // Start the server
