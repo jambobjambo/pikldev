52358d9a38aac78b0e2a1e4178be9e9e46549211
jambobjambo <jamie.a.mcinally@gmail.com>
2016-10-28 12:40:45 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -19,14 +19,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req,res){
-    PythonShell.run('../../piklimageprediction/label.py', options, function (err, results) {
+    /*PythonShell.run('../../piklimageprediction/label.py', options, function (err, results) {
         if (err) throw err;
         // results is an array consisting of messages collected during execution
         console.log('results: %j', results);
-    });
+    });*/
     //var image = req.query.image
-    //res.setHeader('Content-Type', 'application/json');
-    //res.send(JSON.stringify({ 'product' : 'Mens T shirt' }));
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ 'product' : 'Mens T shirt' }));
 });
 
 // Start the server
